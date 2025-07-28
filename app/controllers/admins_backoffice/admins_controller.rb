3a7cf36254509508f1caffc8d14927915f4fd879
rafaellore <birdneon3@gmail.com>
2025-07-28 12:37:40 -0300

    feat(admins_backoffice): adds form_with on  admins edit view

--- app/controllers/admins_backoffice/admins_controller.rb
+++ app/controllers/admins_backoffice/admins_controller.rb
@@ -4,6 +4,17 @@ class AdminsBackoffice::AdminsController < AdminsBackofficeController
   end
 
   def edit
-    # @admin = Admin.find(params[:id])
+    @admin = Admin.find(params[:id])
+  end
+
+  def update
+    admin = Admin.find(params[:id])
+    params_admin = params.require(:admin).permit(:email, :password, :password_confirmation)
+
+    if admin.update(params_admin)
+      redirect_to admins_backoffice_admins_path, notice: "Administrador atualizado com sucesso!"
+    else
+      render :edit
+    end
   end
 end
