9fa86d48b7957a8d8de9d22ccf94ea9d2024b81b
rafaellore <birdneon3@gmail.com>
2025-07-29 11:35:44 -0300

    feat(admins_backoffice): adds delete method

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,7 @@ Rails.application.routes.draw do
 
   namespace :admins_backoffice do
     get "welcome/index" # Dashboard
-    resources :admins,  except: [ :destroy ] # Administradores
+    resources :admins # Administradores
   end
 
   devise_for :admins
