87ddf9e0454f65a9917a3130093abcd3ca4d05f8
rafaellore <birdneon3@gmail.com>
2025-08-13 18:04:12 -0300

    feat(admin_devise): adds admins devise login layout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,16 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+
+  layout :layout_by_resource
+
+   protected
+
+    def layout_by_resource
+      if devise_controller? && resource_class == Admin
+        "admin_devise"
+      else
+        "application"
+      end
+    end
 end
